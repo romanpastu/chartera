0c68c12be7d924b96200d1bb0ef6554f094d3ed2
romanpastu <roman.pastushenko.1997@gmail.com>
2021-06-04 21:47:33 +0200

    basic candles example

--- src/Chart/Canvas.tsx
+++ src/Chart/Canvas.tsx
@@ -1,25 +1,77 @@
-import  { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 
-const draw = (ctx: any)  => {
-    ctx.fillStyle = '#000000'
-    ctx.beginPath()
-    ctx.arc(50, 100, 20, 0, 2*Math.PI)
-    ctx.fill()
-  }
+const drawCandle = (
+  ctx: any,
+  mL: any,
+  mT: any,
+  width: any,
+  height: any,
+  color: string
+) => {
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  //margin left, margin top, width, height
+  ctx.rect(mL, mT, width, height);
+  ctx.fill();
+};
 
+const drawLine = (
+  ctx: any,
+  mL: any,
+  mT: any,
+  width: any,
+  height: any,
+  color: string
+) => {
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  ctx.rect(mL, mT, 1, height);
+  ctx.fill();
+};
 
 const Canvas = (props: any) => {
+  const [data, setData] = useState([
+    {
+      mT: 0,
+      height: 100,
+    },
+    {
+      mT: 35,
+      height: 100,
+    },
+    {
+      mT: 70,
+      height: 100,
+    },
+  ]);
+
+  
   const canvasRef = useRef(null);
   useEffect(() => {
-    const canvas : any  = canvasRef.current;
-    const context =  canvas.getContext("2d") 
+    const canvas: any = canvasRef.current;
+    const context = canvas.getContext("2d");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
     //Our first draw
-    context.fillStyle = "#000000";
+    context.fillStyle = "black";
     context.fillRect(0, 0, context.canvas.width, context.canvas.height);
-
+    //total price range
+    let high = 1000;
+    let low = 0;
+    //accumulatedWidth
+    let accumulatedWith = 0;
+    let candleWidth = 30
     //Our draw come here
-    draw(context)
-  }, [draw]);
+    data.map((i: any, index) => {
+      if (index > 0) {
+        accumulatedWith += candleWidth;
+      }
+
+      console.log((accumulatedWith+i.width)/2)
+      drawCandle(context, accumulatedWith, i.mT, candleWidth, 100, "red");
+      drawLine(context, index > 0 ? accumulatedWith+(candleWidth/2) : (accumulatedWith+candleWidth)/2, i.mT, 1, 140, "red");
+    });
+  }, [drawCandle, drawLine]);
 
   return <canvas ref={canvasRef} {...props} />;
 };
