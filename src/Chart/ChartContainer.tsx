67f8f03cb977f820a38576cf4e4d9167547b6007
romanpastu <roman.pastushenko.1997@gmail.com>
2021-06-23 00:40:46 +0200

    type fix

--- src/Chart/ChartContainer.tsx
+++ src/Chart/ChartContainer.tsx
@@ -3,20 +3,17 @@ import axios from 'axios';
 import React from 'react';
 import Canvas from "./Canvas"
 import { convertData } from "./Helpers"
-type IState = {
-}
 
-type IProps = {  
-}
 
-const ChartContainer : React.FunctionComponent<IState & IProps> = () => {
+const ChartContainer: React.FC = () => {
   const [data, setData] = React.useState<unknown>([])
   const [prevMonthStamp, setPrevMonthStamp] = React.useState<number>()
+
   React.useEffect(() => {
-    var d : any = new Date();
+    var d: any = new Date();
     d.setMonth(d.getMonth() - 2);
     d.setHours(0, 0, 0, 0);
-    setPrevMonthStamp((d/1000|0)*1000); //timestamp of a month ago
+    setPrevMonthStamp((d / 1000 | 0) * 1000); //timestamp of a month ago
   }, [])
 
 
@@ -24,8 +21,8 @@ const ChartContainer : React.FunctionComponent<IState & IProps> = () => {
     axios({
       method: 'get',
       url: 'http://binance.com/api/v3/klines?symbol=BTCUSDT&interval=1d'
-    }).then( res => {
-      if(prevMonthStamp){
+    }).then(res => {
+      if (prevMonthStamp) {
         setData(res.data.filter((i: number[]) => i[0] >= prevMonthStamp))
       }
     }).catch(err => {
@@ -34,14 +31,13 @@ const ChartContainer : React.FunctionComponent<IState & IProps> = () => {
   }, [prevMonthStamp])
 
 
-  if(Array.isArray(data) && data?.length > 0){
+  if (Array.isArray(data) && data?.length > 0) {
     return (
       <Canvas data={convertData(data)} />
     );
-  }else{
+  } else {
     return null
   }
-  
-}
 
+}
 export default ChartContainer;
