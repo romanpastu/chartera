73c64eef8ece75e3015fde0408fd686ab77f444a
romanpastu <roman.pastushenko.1997@gmail.com>
2021-06-27 14:30:16 +0200

    constant change

--- src/Chart/constants.ts
+++ src/Chart/constants.ts
@@ -1,3 +1,3 @@
-export const ChartRightMargin :number  = 50;
+export const ChartRightMargin :number  = 53;
 export const horizontalPriceLines : number =13;
 export const verticalPriceLines : number = 3;
